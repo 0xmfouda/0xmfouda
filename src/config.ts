0b43ecf0c91cf3281064975562dba5f151f8463b
0xmbadr <Muhammad_Fouda@proton.me>
2025-10-19 07:37:33 +0300

    added basics layout and MainGridLayout

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,12 @@
-import type { SiteConfig } from "./types/config";
+import type { ProfileConfig, SiteConfig } from "./types/config";
 
 export const siteConfig: SiteConfig = {
   title: "0xmfouda",
   subtitle: "Portfolio",
   lang: "en",
+  favicon: [],
+};
+
+export const profileConfig: ProfileConfig = {
+  name: "Mohamed Fouda",
 };
--- src/types/config.ts
+++ src/types/config.ts
@@ -2,4 +2,15 @@ export type SiteConfig = {
   title: string;
   subtitle: string;
   lang: "en" | "ar";
+  favicon: Favicon[];
+};
+
+export type ProfileConfig = {
+  name: string;
+};
+
+export type Favicon = {
+  src: string;
+  theme?: "light" | "dark";
+  sizes?: string;
 };
